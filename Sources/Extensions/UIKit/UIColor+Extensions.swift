9641390798d98ce240de728b22a4b2dd5c3e46b7
donghyun <hbull@hanmail.net>
2021-06-20 12:10:06 +0900

    UIColor+Extensions public 적용

--- Sources/Extensions/UIKit/UIColor+Extensions.swift
+++ Sources/Extensions/UIKit/UIColor+Extensions.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 extension UIColor {
-    convenience init(r: Int, g: Int, b: Int, alpha: CGFloat = 1) {
+    public convenience init(r: Int, g: Int, b: Int, alpha: CGFloat = 1) {
         self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: alpha)
     }
     
-    convenience init(rgb: Int, alpha: CGFloat = 1) {
+    public convenience init(rgb: Int, alpha: CGFloat = 1) {
         self.init(red: CGFloat(rgb) / 255, green: CGFloat(rgb) / 255, blue: CGFloat(rgb) / 255, alpha: alpha)
     }
 }
