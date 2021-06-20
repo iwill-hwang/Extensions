87b1c5b2944453fe48a7e16e10641a8bc3da5f4d
donghyun <hbull@hanmail.net>
2021-06-20 12:06:03 +0900

    UIColor+Extensions 추가

---
+++ Sources/Extensions/UIKit/UIColor+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by donghyun on 2021/06/20.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(r: Int, g: Int, b: Int, alpha: CGFloat = 1) {
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: alpha)
+    }
+    
+    convenience init(rgb: Int, alpha: CGFloat = 1) {
+        self.init(red: CGFloat(rgb) / 255, green: CGFloat(rgb) / 255, blue: CGFloat(rgb) / 255, alpha: alpha)
+    }
+}
