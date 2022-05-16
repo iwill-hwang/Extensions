398c12a76ba50742957e753f3e13f5126f0be6db
donghyun <hbull@hanmail.net>
2022-05-16 17:31:33 +0900

    Date+Localization 추가

---
+++ Sources/Extensions/Foundation/Date+Localization.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by donghyun on 2022/05/16.
+//
+
+import Foundation
+
+public enum DateLocalizedFormat {
+    case year
+    case year_month
+    case year_motnth_day
+    case month
+    case month_day
+    case month_day_weekday
+    case day
+    
+    var template: String {
+        switch self {
+        case .year:                     return "yyyy"
+        case .year_month:               return "MMMMyyyy"
+        case .year_motnth_day:          return "dMMMMyyyy"
+        case .month:                    return "MMM"
+        case .month_day:                return "dMMM"
+        case .month_day_weekday:        return "dMMMEEEE"
+        case .day:                      return "d"
+        }
+    }
+}
+
+public extension Date {
+    func localized(with format: DateLocalizedFormat) -> String {
+        let format = DateFormatter.dateFormat(fromTemplate: format.template, options: 0, locale: Locale.current)!
+        let formatter = DateFormatter(with: format)
+        return formatter.string(from: self)
+    }
+}
