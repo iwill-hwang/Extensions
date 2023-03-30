3942d6857f815c87e5e3a68bd335e416ff3b8d6e
iwill.h <iwill.h@kakaocommerce.com>
2023-03-30 16:56:42 +0900

    UIAlertController.Action 추가

--- Sources/Extensions/UIKit/UIViewController+Extensions.swift
+++ Sources/Extensions/UIKit/UIViewController+Extensions.swift
@@ -1,11 +1,30 @@
 #if canImport(UIKit)
 import UIKit
 
+public extension UIAlertController {
+    enum Action {
+        case ok
+        case cancel
+        case custom(String)
+
+        var message: String {
+            switch self {
+            case .ok:
+                return NSLocalizedString("Ok", comment: "Ok")
+            case .cancel:
+                return NSLocalizedString("Cancel", comment: "Cancel")
+            case .custom(let description):
+                return description
+            }
+        }
+    }
+}
+
 public extension UIViewController {
-    func alert(title: String?, message: String?, completion: (() -> (Void))? = nil) {
+    func alert(title: String?, message: String?, ok: UIAlertController.Action = .ok, completion: (() -> (Void))? = nil) {
         let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let ok = UIAlertAction(title: NSLocalizedString("Ok", comment: "Ok"), style: .default, handler: { _ in
+
+        let ok = UIAlertAction(title: ok.message, style: .default, handler: { _ in
             completion?()
         })
         
@@ -14,20 +33,23 @@ public extension UIViewController {
         self.present(controller, animated: true)
     }
     
-    func confirmAlert(title: String?, message: String?, completion:((Bool) -> Void)? = nil) {
+    func confirmAlert(title: String?, message: String?,
+                      ok: UIAlertController.Action = .ok,
+                      cancel: UIAlertController.Action = .cancel,
+                      completion:((Bool) -> Void)? = nil) {
         let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let ok = UIAlertAction(title: NSLocalizedString("Ok", comment: "Ok"), style: .default) { _ in
+
+        let ok = UIAlertAction(title: ok.message, style: .default) { _ in
             completion?(true)
         }
-        
-        let cancel = UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel"), style: .default) { _ in
+
+        let cancel = UIAlertAction(title: cancel.message, style: .default) { _ in
             completion?(false)
         }
-        
+
         controller.addAction(cancel)
         controller.addAction(ok)
-        
+
         self.present(controller, animated: true)
     }
     
